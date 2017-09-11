3d882e85cb573c3cbdff47f9285f81b242541a99
David Larson <dplarson@ucsd.edu>
2017-09-10 22:52:28 -0700

    rnn: add example that follows forecast data organization
    Add working example of an RNN model for time-series prediction, which
    follows the data organization I normally use in forecasting tasks
    (pandas DataFrame, lagged values as columns, etc.).

--- rnn.py
+++ rnn.py
@@ -3,24 +3,112 @@ Use Recurrent Neural Networks to predict a time-series.
 '''
 
 import torch
-from torch import nn
+from torch import nn, optim
+from torch.autograd import Variable
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
 
-input_size = 10     # number of input sequences
-hidden_size = 20    # size of each hidden layer
-num_layers = 2      # number of hidden layers
-num_directions = 1  # number of directions
-batch_size = 3      # number of samples per batch
-seq_len = 5         # length of each input sequence
+# create synthetic data for testing
+# - target: sin(t)
+# - input: corrupted sin(t) signals = sin(t) + noise
+# - organization: match pandas DataFrame
+#
+t = np.linspace(0, 10 * np.pi, 1000)
+y = np.sin(t)
+x1 = y + (np.random.rand(y.shape[0]) - 0.5)
+x2 = y + (np.random.rand(y.shape[0]) - 0.5)
+x3 = y + (np.random.rand(y.shape[0]) - 0.5)
 
-# create RNN model
-model = nn.RNN(input_size, hidden_size, num_layers)
-print model
+data = pd.DataFrame({'y': y, 'x1': x1, 'x2': x2, 'x3': x3})
 
-# create synthetic data
-inputs = torch.autograd.Variable(torch.randn(seq_len, batch_size, input_size))
-h0 = torch.autograd.Variable(torch.randn(num_layers * num_directions, batch_size, hidden_size))
-print inputs.size(), h0.size()
+# created features matrix (lagged values)
+feature_cols = ['x1', 'x2', 'x3']
+features = pd.concat([
+    data[feature_cols].shift(0),
+    data[feature_cols].shift(1),
+    data[feature_cols].shift(2),
+    data[feature_cols].shift(3),
+    data[feature_cols].shift(4),
+], axis=1)
 
-output, hn = model(inputs, h0)
+df = pd.merge(data[['y']], features, how='inner', left_index=True, right_index=True)
 
-print output.size(), hn.size()
+# drop rows with missing data
+df = df.dropna(how='any')
+
+# select variables
+inputs = df[['x1', 'x2', 'x3']].values
+target = df['y'].values
+
+hidden_size = 20  # size of each hidden layer (including output hidden layer)
+num_layers = 2    # number of hidden layers
+
+# number of input sequences
+input_size = len(feature_cols)
+
+# number of output sequences
+output_size = 1
+
+# length of each input sequence
+seq_len = df['x1'].shape[1]
+
+# number of samples per batch
+batch_size = df.shape[0]  # number of samples per batch
+
+#print "Input size:", input_size
+#print "Sequence length:", seq_len
+#print "Hidden layer size:", hidden_size
+
+#==================================================
+# re-shape inputs to required shape
+# - before: (batch_size, seq_len * input_size)
+# - after: (seq_len, batch_size, input_size)
+inputs = inputs.reshape(batch_size, seq_len, input_size)
+#inputs = np.swapaxes(inputs, 0, 1)
+
+# convert to torch data types
+inputs = torch.autograd.Variable(torch.Tensor(inputs))
+target = torch.autograd.Variable(torch.Tensor(target))
+
+
+# RNN model (many-to-one)
+class RNN(nn.Module):
+    def __init__(self, input_size, hidden_size, num_layers, output_size):
+        super(RNN, self).__init__()
+        self.hidden_size = hidden_size
+        self.num_layers = num_layers
+        self.lstm = nn.LSTM(input_size, hidden_size, num_layers, batch_first=True)
+        self.fc = nn.Linear(hidden_size, output_size)
+
+    def forward(self, x):
+        # Set initial states
+        h0 = Variable(torch.zeros(self.num_layers, x.size(0), self.hidden_size))
+        c0 = Variable(torch.zeros(self.num_layers, x.size(0), self.hidden_size))
+
+        # Forward propogate RNN
+        output, _ = self.lstm(x, (h0, c0))
+
+        # Decode hidden state of last time step
+        output = self.fc(output[:, -1, :])
+        return output
+
+
+# Initialize model
+rnn = RNN(input_size, hidden_size, num_layers, output_size)
+
+# Loss and Optimizer
+criterion = nn.MSELoss()
+optimizer = optim.Adam(rnn.parameters(), lr=1e-3)
+
+# Train the model
+num_epochs = 15
+for epoch in range(num_epochs):
+
+    # Forward + Backward + Optimize
+    optimizer.zero_grad()
+    outputs = rnn(inputs)
+    loss = criterion(outputs, target)
+    loss.backward()
+    optimizer.step()
+    print epoch, loss.data[0]
